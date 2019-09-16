23ca59b3008d882ce3b00eef73a46df3fd66970a
Andrew <8879789+manleyac@users.noreply.github.com>
2019-09-16 19:10:07 -0400

    Updates

---
+++ precache-manifest.24d788c55ad8ea1878b26afcc3aef5a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0330439cb85cbeb885be2b5803918912",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "b5905ca8d6d0309d2d06",
+    "url": "/pokemon-app/static/css/main.0c99edef.chunk.css"
+  },
+  {
+    "revision": "7cbc5183dc8cd8c668bb",
+    "url": "/pokemon-app/static/js/2.2f5a5f2b.chunk.js"
+  },
+  {
+    "revision": "b5905ca8d6d0309d2d06",
+    "url": "/pokemon-app/static/js/main.4e4fb770.chunk.js"
+  },
+  {
+    "revision": "a8cc6683b427850c47d9",
+    "url": "/pokemon-app/static/js/runtime~main.3c953706.js"
+  }
+]);
